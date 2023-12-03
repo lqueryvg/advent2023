fcc2c3eaf6b8e4bfaae043e551d57dd74e7b55b6
John Buxton <lqueryvg@gmail.com>
2023-12-03 16:08:44 +0000

    day1 done

--- day01/src/part1.ts
+++ day01/src/part1.ts
@@ -3,17 +3,40 @@ import { readFileSync } from 'fs'
 const getInput = (filename: string) => {
   const file = readFileSync(filename, 'utf-8').trim()
   const lines = file.split(/\r\n|\n/)
-  // or other processing...
-  // for (const line of lines) {
-  //   if (line === '') continue // skip blank lines
-  // }
   return lines
+}
+
+const isDigit = (character: string) => {
+  if (character.length === 1) {
+    const code = character.codePointAt(0)
+    return 47 < code! && code! < 58
+  }
+  return false
+}
+
+const getDigitValue = (character: string) => {
+  const code = character.codePointAt(0)
+  return code! - 48
 }
 
 const main = () => {
   const data = getInput('input.txt')
+  let total = 0
+  for (const line of data) {
+    let lineResult = 0
+    let lastDigit = 0
+    for (const c of line) {
+      if (isDigit(c)) {
+        const thisDigitValue = getDigitValue(c)
+        if (lineResult === 0) lineResult = thisDigitValue * 10
+        lastDigit = thisDigitValue
+      }
+    }
+    lineResult += lastDigit
+    total += lineResult
+  }
 
-  return 'TBD'
+  return total
 }
 
 export { main }
--- day01/src/part2.ts
+++ day01/src/part2.ts
@@ -3,17 +3,59 @@ import { readFileSync } from 'fs'
 const getInput = (filename: string) => {
   const file = readFileSync(filename, 'utf-8').trim()
   const lines = file.split(/\r\n|\n/)
-  // or other processing...
-  // for (const line of lines) {
-  //   if (line === '') continue // skip blank lines
-  // }
   return lines
+}
+
+const numbers: Record<string, number> = {
+  '1': 1,
+  '2': 2,
+  '3': 3,
+  '4': 4,
+  '5': 5,
+  '6': 6,
+  '7': 7,
+  '8': 8,
+  '9': 9,
+  one: 1,
+  two: 2,
+  three: 3,
+  four: 4,
+  five: 5,
+  six: 6,
+  seven: 7,
+  eight: 8,
+  nine: 9,
+}
+
+const stringToNumber = (s: string) => {
+  return numbers[s]
 }
 
 const main = () => {
   const data = getInput('input.txt')
+  let total = 0
+  for (const line of data) {
+    // console.log({ line })
 
-  return 'TBD'
+    let result: RegExpExecArray | null = null
+    let lineResult = 0
+
+    result =
+      /^.*?([1-9]|one|two|three|four|five|six|seven|eight|nine).*([1-9]|one|two|three|four|five|six|seven|eight|nine).*?$/.exec(
+        line
+      )
+    if (result) {
+      lineResult = 10 * stringToNumber(result[1]) + stringToNumber(result[2])
+    } else {
+      result = /([1-9]|one|two|three|four|five|six|seven|eight|nine).*?$/.exec(
+        line
+      )
+      lineResult = 10 * stringToNumber(result![1]) + stringToNumber(result![1])
+    }
+    total += lineResult
+  }
+
+  return total
 }
 
 export { main }
