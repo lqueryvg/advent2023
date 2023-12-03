485ba91c43251d3658df55ee7d869a7e228cf538
John Buxton <lqueryvg@gmail.com>
2023-12-03 21:50:51 +0000

    day3 done

--- day03/src/part1.ts
+++ day03/src/part1.ts
@@ -10,10 +10,80 @@ const getInput = (filename: string) => {
   return lines
 }
 
-const main = () => {
-  const data = getInput('input.txt')
+const isDigit = (character: string) => {
+  // if (!character) return false
+  // console.log({ character })
+  const code = character.codePointAt(0)
+  return 47 < code! && code! < 58
+}
 
-  return 'TBD'
+const isSymbol = (character: string) => {
+  return character !== '.' && !isDigit(character)
+}
+
+const getDigitValue = (character: string) => {
+  const code = character.codePointAt(0)
+  return code! - 48
+}
+
+const main = () => {
+  const lines = getInput('input.txt')
+
+  // console.log({ h: lines.length, w: lines[0].length })
+
+  const getCharAt = (x: number, y: number) => {
+    if (y < 0 || y + 1 >= lines.length || x < 0 || x + 1 >= lines[0].length)
+      return '.'
+    return lines[y][x]
+  }
+
+  const hasSymbolNeighbor = (x: number, y: number) => {
+    return (
+      isSymbol(getCharAt(x - 1, y - 1)) ||
+      isSymbol(getCharAt(x - 1, y)) ||
+      isSymbol(getCharAt(x - 1, y + 1)) ||
+      isSymbol(getCharAt(x + 1, y - 1)) ||
+      isSymbol(getCharAt(x + 1, y)) ||
+      isSymbol(getCharAt(x + 1, y + 1)) ||
+      isSymbol(getCharAt(x, y - 1)) ||
+      isSymbol(getCharAt(x, y + 1))
+    )
+  }
+
+  let total = 0
+  let y = 0
+  for (const line of lines) {
+    let x = 0
+    let number: number = 0
+    let symbolNeighborFound = false
+    for (const character of line) {
+      // console.log({ y, x, character })
+      if (isDigit(character)) {
+        number = number * 10 + getDigitValue(character)
+        if (!symbolNeighborFound) {
+          symbolNeighborFound = hasSymbolNeighbor(x, y)
+        }
+      } else {
+        if (symbolNeighborFound && number > 0) {
+          // console.log({ number, y, x })
+          total += number
+        } else {
+          // if (number > 0) console.log({ x, y, number })
+        }
+        number = 0
+        symbolNeighborFound = false
+      }
+      // console.log({ symbolNeighborFound })
+      x++
+    }
+    if (symbolNeighborFound && number > 0) {
+      // console.log({ number, y, x })
+      total += number
+    }
+    y++
+  }
+
+  return total
 }
 
 export { main }
--- day03/src/part2.ts
+++ day03/src/part2.ts
@@ -10,10 +10,101 @@ const getInput = (filename: string) => {
   return lines
 }
 
-const main = () => {
-  const data = getInput('input.txt')
+const isDigit = (character: string) => {
+  // if (!character) return false
+  // console.log({ character })
+  const code = character.codePointAt(0)
+  return 47 < code! && code! < 58
+}
 
-  return 'TBD'
+const isSymbol = (character: string) => {
+  return character !== '.' && !isDigit(character)
+}
+const isGear = (character: string) => {
+  return character === '*'
+}
+
+const getDigitValue = (character: string) => {
+  const code = character.codePointAt(0)
+  return code! - 48
+}
+
+const main = () => {
+  const lines = getInput('input.txt')
+
+  const getCharAt = (x: number, y: number) => {
+    if (y < 0 || y + 1 >= lines.length || x < 0 || x + 1 >= lines[0].length)
+      return '.'
+    return lines[y][x]
+  }
+
+  const hasSymbolNeighbor = (x: number, y: number) => {
+    return (
+      isSymbol(getCharAt(x - 1, y - 1)) ||
+      isSymbol(getCharAt(x - 1, y)) ||
+      isSymbol(getCharAt(x - 1, y + 1)) ||
+      isSymbol(getCharAt(x + 1, y - 1)) ||
+      isSymbol(getCharAt(x + 1, y)) ||
+      isSymbol(getCharAt(x + 1, y + 1)) ||
+      isSymbol(getCharAt(x, y - 1)) ||
+      isSymbol(getCharAt(x, y + 1))
+    )
+  }
+
+  const getGearLocation = (x: number, y: number) => {
+    if (isGear(getCharAt(x - 1, y - 1))) return `${y - 1},${x - 1}`
+    if (isGear(getCharAt(x - 1, y))) return `${y},${x - 1}`
+    if (isGear(getCharAt(x - 1, y + 1))) return `${y + 1},${x - 1}`
+    if (isGear(getCharAt(x + 1, y - 1))) return `${y - 1},${x + 1}`
+    if (isGear(getCharAt(x + 1, y))) return `${y},${x + 1}`
+    if (isGear(getCharAt(x + 1, y + 1))) return `${y + 1},${x + 1}`
+    if (isGear(getCharAt(x, y - 1))) return `${y - 1},${x}`
+    if (isGear(getCharAt(x, y + 1))) return `${y + 1},${x}`
+    return ''
+  }
+
+  const gears: Record<string, number[]> = {}
+
+  const addGearNumber = (gearLocation: string, number: number) => {
+    if (gearLocation === '') return
+    if (gears[gearLocation] === undefined) {
+      gears[gearLocation] = [number]
+      return
+    }
+    gears[gearLocation].push(number)
+  }
+
+  let y = 0
+  for (const line of lines) {
+    let x = 0
+    let number: number = 0
+    let gearLocation = ''
+    for (const character of line) {
+      if (isDigit(character)) {
+        number = number * 10 + getDigitValue(character)
+        if (gearLocation === '') {
+          gearLocation = getGearLocation(x, y)
+        }
+      } else {
+        addGearNumber(gearLocation, number)
+        number = 0
+        gearLocation = ''
+      }
+      x++
+    }
+    addGearNumber(gearLocation, number)
+    y++
+  }
+
+  let total = 0
+
+  for (const gearLocation in gears) {
+    if (gears[gearLocation].length !== 1) {
+      total += gears[gearLocation][0] * gears[gearLocation][1]
+    }
+  }
+
+  return total
 }
 
 export { main }
