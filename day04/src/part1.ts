322155a1ea2fe58ef9190d757cbb312ef765424e
John Buxton <lqueryvg@gmail.com>
2023-12-04 11:49:38 +0000

    day 4 done

--- day04/src/part1.ts
+++ day04/src/part1.ts
@@ -10,10 +10,33 @@ const getInput = (filename: string) => {
   return lines
 }
 
-const main = () => {
-  const data = getInput('input.txt')
+const intersect = (
+  setA: Set<string>,
+  setB: Set<string>,
+  ...args: Set<string>[]
+): Set<string> => {
+  const result = new Set([...setA].filter((i) => setB.has(i)))
+  if (args.length === 0) return result
+  return intersect(result, args.shift()!, ...args)
+}
 
-  return 'TBD'
+const main = () => {
+  const lines = getInput('input.txt')
+  let total = 0
+  for (const line of lines) {
+    const [_, cardContents] = line.split(/: +/)
+    const [winningNumbersString, myNumbersString] = cardContents.split(/ +\| +/)
+    const winningNumbers = new Set(winningNumbersString.split(/ +/))
+    const myNumbers = new Set(myNumbersString.split(' '))
+    const myWinningNumbers = intersect(winningNumbers, myNumbers)
+    const numWinners = myWinningNumbers.size
+    // console.log({ line, numWinners })
+    if (numWinners > 0) {
+      total += Math.pow(2, numWinners - 1)
+    }
+  }
+
+  return total
 }
 
 export { main }
--- day04/src/part2.ts
+++ day04/src/part2.ts
@@ -10,10 +10,58 @@ const getInput = (filename: string) => {
   return lines
 }
 
-const main = () => {
-  const data = getInput('input.txt')
+const intersect = (
+  // https://stackoverflow.com/a/70291510/2964461
+  setA: Set<string>,
+  setB: Set<string>,
+  ...args: Set<string>[]
+): Set<string> => {
+  const result = new Set([...setA].filter((i) => setB.has(i)))
+  if (args.length === 0) return result
+  return intersect(result, args.shift()!, ...args)
+}
 
-  return 'TBD'
+// const cardCounts: Record<string, number> = {}
+const cardCounts: number[] = []
+
+const addToCardCount = (cardNumber: number, add: number) => {
+  // console.log('incrementing', { cardNumber })
+  if (cardCounts[cardNumber - 1] === undefined) {
+    cardCounts[cardNumber - 1] = add
+  } else {
+    cardCounts[cardNumber - 1] += add
+  }
+}
+
+const main = () => {
+  const lines = getInput('input.txt')
+
+  let cardNumber = 1
+  for (const line of lines) {
+    const [_, cardContents] = line.split(/: +/)
+
+    const [winningNumbersString, myNumbersString] = cardContents.split(/ +\| +/)
+    const winningNumbers = new Set(winningNumbersString.split(/ +/))
+    const myNumbers = new Set(myNumbersString.split(' '))
+
+    const myWinningNumbers = intersect(winningNumbers, myNumbers)
+    addToCardCount(cardNumber, 1)
+    const numWinners = myWinningNumbers.size
+    let nextCardNumber = cardNumber + 1
+    while (
+      nextCardNumber <= cardNumber + numWinners &&
+      nextCardNumber <= lines.length
+    ) {
+      addToCardCount(nextCardNumber, cardCounts[cardNumber - 1])
+      nextCardNumber++
+    }
+    cardNumber++
+  }
+
+  return cardCounts.reduce(
+    (accumulator, currentValue) => accumulator + currentValue,
+    0
+  )
 }
 
 export { main }
