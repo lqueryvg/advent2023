f6481b97a3b38b023030ed23935074275b7f64d5
John Buxton <lqueryvg@gmail.com>
2023-12-09 22:30:13 +0000

    day 6 done

--- day06/src/part1.ts
+++ day06/src/part1.ts
@@ -4,16 +4,55 @@ const getInput = (filename: string) => {
   const file = readFileSync(filename, 'utf-8').trim()
   const lines = file.split(/\r\n|\n/)
   // or other processing...
-  // for (const line of lines) {
-  //   if (line === '') continue // skip blank lines
-  // }
-  return lines
+  type Race = { time: number; distance: number }
+  const raceTimes: number[] = []
+  const raceRecords: number[] = []
+  for (const line of lines) {
+    if (line.match(/Time:/)) {
+      const timesText = line.split(/\s+/).values()
+      timesText.next()
+      for (const timeText of timesText) {
+        raceTimes.push(parseInt(timeText))
+      }
+    }
+    if (line.match(/Distance:/)) {
+      const recordsText = line.split(/\s+/).values()
+      recordsText.next()
+      for (const timeText of recordsText) {
+        raceRecords.push(parseInt(timeText))
+      }
+    }
+
+    // if (line === '') continue // skip blank lines
+  }
+  return raceTimes.map((raceTime, i) => {
+    return { time: raceTime, record: raceRecords[i] }
+  })
+  // return lines
 }
 
 const main = () => {
-  const data = getInput('input.txt')
+  const races = getInput('input.txt')
+  // console.log({ races })
 
-  return 'TBD'
+  const ways: number[] = []
+
+  for (const race of races) {
+    // console.log({ race })
+    let numWays = 0
+    for (let time = 0; time <= race.time; time++) {
+      const distance = time * (race.time - time)
+      const recordBroken = distance > race.record
+      // console.log({ time, distance, recordBroken })
+      if (recordBroken) {
+        numWays++
+      }
+    }
+
+    ways.push(numWays)
+  }
+
+  return ways.reduce((a, b) => a * b)
 }
 
 export { main }
--- day06/src/part2.ts
+++ day06/src/part2.ts
@@ -4,16 +4,55 @@ const getInput = (filename: string) => {
   const file = readFileSync(filename, 'utf-8').trim()
   const lines = file.split(/\r\n|\n/)
   // or other processing...
-  // for (const line of lines) {
-  //   if (line === '') continue // skip blank lines
-  // }
-  return lines
+  type Race = { time: number; distance: number }
+  const raceTimes: number[] = []
+  const raceRecords: number[] = []
+  for (const line of lines) {
+    if (line.match(/Time:/)) {
+      const timesText = line.split(/\s+/).values()
+      timesText.next()
+      raceTimes.push(parseInt([...timesText].join('')))
+    }
+    if (line.match(/Distance:/)) {
+      const recordsText = line.split(/\s+/).values()
+      recordsText.next()
+      raceRecords.push(parseInt([...recordsText].join('')))
+    }
+  }
+  return raceTimes.map((raceTime, i) => {
+    return { time: raceTime, record: raceRecords[i] }
+  })
 }
 
 const main = () => {
-  const data = getInput('input.txt')
+  const races = getInput('input.txt')
 
-  return 'TBD'
+  const ways: number[] = []
+
+  for (const race of races) {
+    const root = Math.sqrt(race.time * race.time - 4 * race.record)
+    const way1 = Math.round((race.time + root) / 2)
+    const way2 = Math.round((race.time - root) / 2)
+
+    const nway1 = Math.max(
+      ...[way1 - 1, way1, way1 + 1].filter((time) => {
+        const distance = time * (race.time - time)
+        return distance > race.record
+      })
+    )
+
+    const nway2 = Math.min(
+      ...[way2 - 1, way2, way2 + 1].filter((time) => {
+        const distance = time * (race.time - time)
+        return distance > race.record
+      })
+    )
+
+    const nways = Math.abs(nway1 - nway2) + 1
+    ways.push(nways)
+  }
+
+  return ways.reduce((a, b) => a * b)
 }
 
 export { main }
