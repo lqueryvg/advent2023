c99c9921d0d6862213332a900143a9b8cabd8d57
John Buxton <lqueryvg@gmail.com>
2023-12-09 20:31:01 +0000

    day 5 done

--- day05/src/part1.ts
+++ day05/src/part1.ts
@@ -3,17 +3,74 @@ import { readFileSync } from 'fs'
 const getInput = (filename: string) => {
   const file = readFileSync(filename, 'utf-8').trim()
   const lines = file.split(/\r\n|\n/)
-  // or other processing...
-  // for (const line of lines) {
-  //   if (line === '') continue // skip blank lines
-  // }
   return lines
 }
 
 const main = () => {
   const data = getInput('input.txt')
 
-  return 'TBD'
+  const lines = data.entries()
+
+  const {
+    value: [_, seedsText],
+  } = lines.next()
+  // console.log({ seedsText })
+
+  const sourceNumbers: number[] = []
+  for (const seedText of seedsText.split(' ')) {
+    if (seedText !== 'seeds:') {
+      sourceNumbers.push(parseInt(seedText))
+    }
+  }
+  // console.log({ sourceNumbers })
+
+  type MapRange = {
+    destStart: number
+    sourceStart: number
+    length: number
+  }
+
+  let ranges: MapRange[]
+
+  const mapNumber = (sourceNumber: number) => {
+    for (const range of ranges) {
+      if (
+        sourceNumber >= range.sourceStart &&
+        sourceNumber <= range.sourceStart + range.length
+      ) {
+        return range.destStart + sourceNumber - range.sourceStart
+      }
+    }
+    return sourceNumber
+  }
+
+  const mapSourceNumbers = () => {
+    sourceNumbers.forEach((sourceNumber: number, index: number) => {
+      sourceNumbers[index] = mapNumber(sourceNumber)!
+    })
+    // console.log({ sourceNumbers })
+  }
+
+  let _skipline = lines.next() // skip blank line
+  for (const [_skip, line] of lines) {
+    if (line.match(/map:/)) {
+      ranges = []
+      continue
+    }
+
+    // console.log({ line })
+    if (line !== '') {
+      const [destStart, sourceStart, length] = line
+        .split(' ')
+        .map((x) => parseInt(x))
+      ranges!.push({ destStart, sourceStart, length })
+    } else {
+      mapSourceNumbers()
+    }
+  }
+  mapSourceNumbers()
+
+  return Math.min(...sourceNumbers)
 }
 
 export { main }
--- day05/src/part2.ts
+++ day05/src/part2.ts
@@ -3,17 +3,112 @@ import { readFileSync } from 'fs'
 const getInput = (filename: string) => {
   const file = readFileSync(filename, 'utf-8').trim()
   const lines = file.split(/\r\n|\n/)
-  // or other processing...
-  // for (const line of lines) {
-  //   if (line === '') continue // skip blank lines
-  // }
   return lines
 }
 
 const main = () => {
   const data = getInput('input.txt')
 
-  return 'TBD'
+  const lines = data.entries()
+
+  const {
+    value: [_, seedsText],
+  } = lines.next()
+
+  type Range = { start: number; end: number }
+
+  let seedRanges: Range[] = []
+
+  const seedTextIterator = seedsText.split(' ').values()
+  for (const seedText of seedTextIterator) {
+    if (seedText == 'seeds:') continue
+    const [start, length] = [
+      parseInt(seedText),
+      parseInt(seedTextIterator.next().value),
+    ]
+    seedRanges.push({ start, end: start + length - 1 })
+  }
+
+  type Mapping = {
+    src: Range
+    dst: Range
+  }
+
+  let targetRanges: Mapping[]
+
+  const newRange = (s: number, e: number) => {
+    return { start: s, end: e }
+  }
+
+  const rangeToString = (r: Range) => {
+    return `${r.start},${r.end}`
+  }
+
+  const doMapping = () => {
+    const newSeedRanges: Record<string, Range> = {}
+    nextSeedRange: for (const seedRange of seedRanges) {
+      for (const targetRange of targetRanges) {
+        const s = seedRange
+        const t = targetRange.src
+        const delta = targetRange.dst.start - targetRange.src.start
+        // entire range is below or above
+        if (s.end < t.start || s.start > t.end) {
+          continue
+        }
+
+        // split bottom
+        if (s.start < t.start && s.end >= t.start) {
+          seedRanges.push(newRange(s.start, t.start - 1))
+        }
+
+        // split top
+        if (s.start <= s.end && s.end > t.end) {
+          seedRanges.push(newRange(t.end + 1, s.end))
+        }
+
+        // overlap
+        const overlapRange = newRange(
+          Math.max(s.start, t.start) + delta,
+          Math.min(s.end, t.end) + delta
+        )
+        newSeedRanges[rangeToString(overlapRange)] = overlapRange
+        continue nextSeedRange
+      }
+      newSeedRanges[rangeToString(seedRange)] = seedRange
+    }
+    seedRanges = [...Object.values(newSeedRanges)]
+  }
+
+  const targetStarts: number[] = []
+  let _skipline = lines.next() // skip blank line
+  for (const [_skip, line] of lines) {
+    if (line.match(/map:/)) {
+      targetRanges = []
+      continue
+    }
+
+    if (line !== '') {
+      // reading map
+      const [destStart, sourceStart, length] = line
+        .split(' ')
+        .map((x) => parseInt(x))
+      targetRanges!.push({
+        src: { start: sourceStart, end: sourceStart + length - 1 },
+        dst: { start: destStart, end: destStart + length - 1 },
+      })
+    } else {
+      doMapping()
+    }
+  }
+  doMapping()
+
+  const lowestLocation = Math.min(
+    ...seedRanges.map((s) => {
+      return s.start
+    })
+  )
+
+  return lowestLocation
 }
 
 export { main }
