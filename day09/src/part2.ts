61fc9ad61b5ab9b72d118a3b8dff85d46eb839b2
John Buxton <lqueryvg@gmail.com>
2023-12-11 08:37:32 +0000

    day 9 done

--- day09/src/part2.ts
+++ day09/src/part2.ts
@@ -10,10 +10,40 @@ const getInput = (filename: string) => {
   return lines
 }
 
-const main = () => {
-  const data = getInput('input.txt')
+const getFirst = (numbers: number[]): number => {
+  return numbers[0]
+}
 
-  return 'TBD'
+const getDifferences = (numbers: number[]): number[] => {
+  const differences: number[] = []
+  for (const [index, value] of numbers.entries()) {
+    if (index === numbers.length - 1) break
+    differences.push(numbers[index + 1] - value)
+  }
+  return differences
+}
+
+const allZero = (numbers: number[]): boolean => {
+  const found = numbers.find((n) => n !== 0)
+  return found === undefined
+}
+
+const getPrevious = (numbers: number[]): number => {
+  if (allZero(numbers)) return 0
+  const differences = getDifferences(numbers)
+  return getFirst(numbers) - getPrevious(differences)
+}
+
+const main = () => {
+  const lines = getInput('input.txt')
+
+  let total = 0
+  for (const line of lines) {
+    const numbers = line.split(' ').map((s) => parseInt(s))
+    total += getPrevious(numbers)
+  }
+
+  return total
 }
 
 export { main }
