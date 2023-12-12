9090bc6c319e6a15155546f6d4151503d083ffe8
John Buxton <lqueryvg@gmail.com>
2023-12-12 18:40:44 +0000

    day 12 done

--- day12/src/part1.ts
+++ day12/src/part1.ts
@@ -3,17 +3,72 @@ import { readFileSync } from 'fs'
 const getInput = (filename: string) => {
   const file = readFileSync(filename, 'utf-8').trim()
   const lines = file.split(/\r\n|\n/)
-  // or other processing...
-  // for (const line of lines) {
-  //   if (line === '') continue // skip blank lines
-  // }
   return lines
 }
 
-const main = () => {
-  const data = getInput('input.txt')
+let springWidth = 1
+const findWays = (springs: string, groupSizes: number[]): number => {
+  // console.log('findWays() called', {
+  //   springs: springs.padStart(springWidth),
+  //   groupSizes,
+  // })
 
-  return 'TBD'
+  if (groupSizes.length === 0) {
+    return springs.match(/#/) ? 0 : 1
+  }
+
+  // strip leading dots
+  const remainingSprings = springs.replace(/^\.+/, '')
+  if (remainingSprings === '') {
+    return 0
+  }
+
+  const remainingGroupSizes = [...groupSizes]
+  const currentGroupSize = remainingGroupSizes.shift()
+  if (currentGroupSize === undefined) {
+    // we've found a place for all groups, so we've found 1 way
+    return 1
+  }
+
+  // get possible group length
+  const matchGroups = remainingSprings.match(/([?#]+)[^.]*/g)
+  if (!matchGroups) {
+    return 0
+  }
+
+  let numWays = 0
+  // try to fit next group
+  if (
+    matchGroups[0].length >= currentGroupSize &&
+    remainingSprings[currentGroupSize] !== '#'
+  ) {
+    // if (remainingGroupSizes.length > 0) {
+    const nextSprings = `${remainingSprings.slice(currentGroupSize + 1)}`
+    numWays += findWays(nextSprings, remainingGroupSizes)
+  }
+
+  // try skipping this character if we can
+  if (remainingSprings[0] === '?') {
+    numWays += findWays(`.${remainingSprings.slice(1)}`, groupSizes)
+  }
+
+  return numWays
+}
+
+const main = () => {
+  const lines = getInput('input.txt')
+
+  let total = 0
+  for (const line of lines) {
+    const [springs, groupsText] = line.split(' ')
+    const groups = groupsText.split(',').map((n) => parseInt(n))
+    springWidth = springs.length
+    const ways = findWays(springs, groups)
+    // console.log({ springs, groups, ways })
+    total += ways
+  }
+
+  return total
 }
 
 export { main }
--- day12/src/part2.ts
+++ day12/src/part2.ts
@@ -3,17 +3,84 @@ import { readFileSync } from 'fs'
 const getInput = (filename: string) => {
   const file = readFileSync(filename, 'utf-8').trim()
   const lines = file.split(/\r\n|\n/)
-  // or other processing...
-  // for (const line of lines) {
-  //   if (line === '') continue // skip blank lines
-  // }
   return lines
 }
 
-const main = () => {
-  const data = getInput('input.txt')
+let springWidth = 1
 
-  return 'TBD'
+const findWays = (springs: string, groupSizes: number[]): number => {
+  // console.log('findWays() called', {
+  //   springs: springs.padStart(springWidth),
+  //   groupSizes,
+  // })
+
+  if (groupSizes.length === 0) {
+    return springs.match(/#/) ? 0 : 1
+  }
+
+  // strip leading dots
+  const remainingSprings = springs.replace(/^\.+/, '')
+  if (remainingSprings === '') {
+    return 0
+  }
+
+  const remainingGroupSizes = [...groupSizes]
+  const currentGroupSize = remainingGroupSizes.shift()
+  if (currentGroupSize === undefined) {
+    // we've found a place for all groups, so we've found 1 way
+    return 1
+  }
+
+  // get possible group length
+  const matchGroups = remainingSprings.match(/([?#]+)[^.]*/g)
+  if (!matchGroups) {
+    return 0
+  }
+
+  let numWays = 0
+  // try to fit next group
+  if (
+    matchGroups[0].length >= currentGroupSize &&
+    remainingSprings[currentGroupSize] !== '#'
+  ) {
+    const nextSprings = `${remainingSprings.slice(currentGroupSize + 1)}`
+    numWays += findWaysCached(nextSprings, remainingGroupSizes)
+  }
+
+  // try skipping this character if we can
+  if (remainingSprings[0] === '?') {
+    numWays += findWaysCached(`.${remainingSprings.slice(1)}`, groupSizes)
+  }
+
+  return numWays
+}
+
+const cache: Record<string, number> = {}
+
+const findWaysCached = (springs: string, groupSizes: number[]): number => {
+  const key = `${springs}:${groupSizes.join(',')}`
+  if (key in cache) return cache[key]
+  const n = findWays(springs, groupSizes)
+  cache[key] = n
+  return n
+}
+
+const main = () => {
+  const lines = getInput('input.txt')
+
+  let total = 0
+  for (const line of lines) {
+    const [springsText, groupsText] = line.split(' ')
+    const springs = Array(5).fill(springsText).join('?')
+    const groupsTextNew = Array(5).fill(groupsText).join(',')
+    const groups = groupsTextNew.split(',').map((n) => parseInt(n))
+    springWidth = springs.length
+    const ways = findWays(springs, groups)
+    // console.log({ springs, groups, ways })
+    total += ways
+  }
+
+  return total
 }
 
 export { main }
