ec5af5b14e84301e1b805d6c276535720bc59389
John Buxton <lqueryvg@gmail.com>
2023-12-15 09:34:31 +0000

    day 15 done

--- day15/src/part1.ts
+++ day15/src/part1.ts
@@ -3,17 +3,26 @@ import { readFileSync } from 'fs'
 const getInput = (filename: string) => {
   const file = readFileSync(filename, 'utf-8').trim()
   const lines = file.split(/\r\n|\n/)
-  // or other processing...
-  // for (const line of lines) {
-  //   if (line === '') continue // skip blank lines
-  // }
   return lines
 }
 
 const main = () => {
-  const data = getInput('input.txt')
+  const lines = getInput('input.txt')
+  const line = lines.shift()!
 
-  return 'TBD'
+  const words = line.split(',')
+  let total = 0
+  for (const word of words) {
+    let wordTotal = 0
+    for (const letter of word) {
+      wordTotal += letter.charCodeAt(0)
+      wordTotal *= 17
+      wordTotal %= 256
+    }
+    total += wordTotal
+  }
+
+  return total
 }
 
 export { main }
--- day15/src/part2.ts
+++ day15/src/part2.ts
@@ -3,17 +3,84 @@ import { readFileSync } from 'fs'
 const getInput = (filename: string) => {
   const file = readFileSync(filename, 'utf-8').trim()
   const lines = file.split(/\r\n|\n/)
-  // or other processing...
-  // for (const line of lines) {
-  //   if (line === '') continue // skip blank lines
-  // }
   return lines
 }
 
-const main = () => {
-  const data = getInput('input.txt')
+const hash = (word: string) => {
+  let wordTotal = 0
+  for (const letter of word) {
+    wordTotal += letter.charCodeAt(0)
+    wordTotal *= 17
+    wordTotal %= 256
+  }
+  return wordTotal
+}
 
-  return 'TBD'
+type Lens = {
+  label: string
+  focalLength: number
+}
+
+const findLensIndex = (lenses: Lens[], label: string): number => {
+  return lenses.findIndex((l: Lens) => l.label === label)
+}
+
+const removeLens = (lenses: Lens[], label: string): Lens[] => {
+  return lenses.filter((l: Lens) => l.label !== label)
+}
+
+// const printBoxes = (boxes: Lens[][]) => {
+//   for (let i = 0; i < boxes.length; i++) {
+//     const box = boxes[i]
+//     if (box.length !== 0) {
+//       console.log({ i, box })
+//     }
+//   }
+// }
+
+const main = () => {
+  const lines = getInput('input.txt')
+  const line = lines.shift()!
+
+  const words = line.split(',')
+  const boxes: Lens[][] = []
+  for (let i = 0; i < 256; i++) {
+    boxes.push([] as Lens[])
+  }
+
+  for (const word of words) {
+    let [label, focalLengthText] = word.split('=')
+
+    if (focalLengthText === undefined) {
+      // operation -
+      label = label.substring(0, label.length - 1)
+      const boxNumber = hash(label)
+      const lenses = boxes[boxNumber]
+      boxes[boxNumber] = removeLens(lenses, label)
+    } else {
+      // operation =
+      const boxNumber = hash(label)
+      const focalLength = parseInt(focalLengthText)
+      const lenses = boxes[boxNumber]
+      const lensIndex = findLensIndex(lenses, label)
+      if (lensIndex === -1) {
+        lenses.push({ label, focalLength })
+      } else {
+        lenses[lensIndex] = { label, focalLength }
+      }
+    }
+  }
+
+  let total = 0
+  for (let boxIndex = 0; boxIndex < 256; boxIndex++) {
+    const lenses = boxes[boxIndex]
+    for (let lensIndex = 0; lensIndex < lenses.length; lensIndex++) {
+      const lens = lenses[lensIndex]
+      total += (1 + boxIndex) * (lensIndex + 1) * lens.focalLength
+    }
+  }
+
+  return total
 }
 
 export { main }
